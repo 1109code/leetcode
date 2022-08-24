fa35a80d82670aa48775e0218648a9a91fe53a51
1109code <109256753+1109code@users.noreply.github.com>
2022-08-24 22:40:31 +0900

    Time: 29 ms (56.90%), Space: 13.2 MB (96.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    dp = collections.defaultdict(int)
+    
+    def fib(self, N):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if N <= 1:
+            return N
+        
+        if self.dp[N]:
+            return self.dp[N]
+        
+        self.dp[N] = self.fib(N-1) + self.fib(N-2)
+        return self.dp[N]
