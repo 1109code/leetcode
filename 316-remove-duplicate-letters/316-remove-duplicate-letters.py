c6312858558b60d3d1c8589e10f35dceef49e000
1109code <109256753+1109code@users.noreply.github.com>
2022-08-29 10:38:37 +0900

    Time: 42 ms (85.36%), Space: 13.9 MB (80.89%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,17 +1,23 @@
-class Solution(object):
-    def removeDuplicateLetters(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        # counter는 문자별 개수를 자동으로 카이팅
+        counter, seen, stack = collections.Counter(s), set(), []
         
-        # 중복을 제거하고 정렬해 순환
-        for char in sorted(set(s)):
-            # 가장 빠른 char의 인덱스부터를 suffix
-            suffix = s[s.index(char):]
-            # set과 suffix가 일치하면
-            if set(s) == set(suffix):
-                # 해당 cahr와 suffix에서 해당 문자를 제거한 suffix를 재귀로 다시 탐색
-                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
-        # 모두 순회하고 나오면 없을땐 그냥 반환
-        return ''
+        # 입력된 문자열을 순회하며
+        for char in s:
+            # 해당하는 counter 개수 하나 감소
+            counter[char] -= 1
+            # 이미 처리한 문자열이면 넘어가고 다시 순환
+            if char in seen:
+                continue
+            
+            # 처음 제외를 위해 stack
+            # 현재 문자가 스택에 쌓인 이전 문자보다 작은데 뒤에 있으면
+            # 이전 문자를 stack과 seen 에서 제거 (뒤에서 다시 넣을 것 이므로)
+            while stack and char < stack[-1] and counter[stack[-1]]  > 0:
+                seen.remove(stack.pop())
+            # 현재문자를 stack 과 seen에 넣기
+            stack.append(char)
+            seen.add(char)
+            
+        return ''.join(stack)
