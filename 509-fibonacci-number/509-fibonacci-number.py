62fab0caf06011101bdae0dc86aebffe1012bc9b
1109code <109256753+1109code@users.noreply.github.com>
2022-09-03 17:51:35 +0900

    Time: 19 ms (87.17%), Space: 13.4 MB (60.66%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,6 +1,19 @@
 class Solution(object):
-    def fib(self, n):
-        x, y = 0, 1
-        for i in range(n):
-            x, y = y, x + y
-        return x
+    dp = collections.defaultdict(int)
+    
+    def fib(self, N):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # N이 1보다 작거나 같으면 그냥 반환
+        if N <= 1:
+            return N
+        
+        # 원하는 dp[N] 값이 있으면 반환
+        if self.dp[N]:
+            return self.dp[N]
+        
+        # 원하는 값이 없으면 재귀로 저장
+        self.dp[N] = self.fib(N-1) + self.fib(N-2)
+        return self.dp[N]
