b9e478518602de3395e819643c15b15ac069d142
1109code <109256753+1109code@users.noreply.github.com>
2022-09-18 16:45:58 +0900

    Time: 176 ms (25.00%), Space: 14.6 MB (60.25%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -1,17 +1,15 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = collections.defaultdict(list)
-        # 그래프 순서대로 구성
-        for a, b in sorted(tickets):
+        # 그래프를 뒤집어서 구성
+        for a, b in sorted(tickets, reverse=True):
             graph[a].append(b)
         
         route = []
         def dfs(a):
-            # 첫 번째 값을 읽어 어휘 순 방문
             while graph[a]:
-                dfs(graph[a].pop(0))
+                dfs(graph[a].pop())
             route.append(a)
-        
+            
         dfs('JFK')
-        # 다쉬 뒤집어 어휘 순 결과로
         return route[::-1]
