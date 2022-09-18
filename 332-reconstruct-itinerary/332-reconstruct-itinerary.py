0504d61bb42c5ca73b746e8964e27998bc415404
1109code <109256753+1109code@users.noreply.github.com>
2022-09-18 17:00:20 +0900

    Time: 125 ms (62.97%), Space: 14.1 MB (99.61%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -1,15 +1,15 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = collections.defaultdict(list)
-        # 그래프를 뒤집어서 구성
-        for a, b in sorted(tickets, reverse=True):
+        # 그래프 순서대로 구성
+        for a, b in sorted(tickets):
             graph[a].append(b)
         
-        route = []
-        def dfs(a):
-            while graph[a]:
-                dfs(graph[a].pop())
-            route.append(a)
+        route, stack = [], ['JFK']
+        while stack:
+            # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop(0))
+            route.append(stack.pop())
             
-        dfs('JFK')
         return route[::-1]
