2bfb06d4223701404d414ae68090017c0fd1ea25
1109code <109256753+1109code@users.noreply.github.com>
2022-09-28 00:02:05 +0900

    Time: 527 ms (22.13%), Space: 23.3 MB (8.78%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -7,15 +7,15 @@
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], L: int, R: int) -> int:
         stack, sum = [root], 0
-        # 스택 이용 필요한 노드 DFS 반복
+        # 큐 연산을 이용해 반복 구조 BFS로 필요한 노드 탐색
+        # BFS로 한거 말고는 달라진게 없음
         while stack:
-            node = stack.pop()
+            node = stack.pop(0)
             if node:
-                if node.val > L:                # 재귀로 왼쪽 오른쪽 자식 탐색
+                if node.val > L:
                     stack.append(node.left)
                 if node.val < R:
                     stack.append(node.right)
-                if L <= node.val <= R:          # 현재 노드 추가
+                if L <= node.val <= R:
                     sum += node.val
-                    
         return sum
