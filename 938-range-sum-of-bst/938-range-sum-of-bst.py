20cd253e68d748be26aa4d7a7967930ad1b94f1b
1109code <109256753+1109code@users.noreply.github.com>
2022-09-27 23:55:07 +0900

    Time: 479 ms (30.82%), Space: 23 MB (91.77%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -6,9 +6,14 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], L: int, R: int) -> int:
-        if not root:            # 예외 처리
-            return 0
+        def dfs(node: TreeNode):
+            if not node:
+                return 0
+            
+            if node.val < L:                # 어차피 왼쪽 가지들은 현재 노드보다 작으므로
+                return dfs(node.right)      # 현재 노드가 L보다 작으면 왼쪽 노드는 탐색 할 필요 X
+            elif node.val > R:
+                return dfs(node.left)       # 동일
+            return node.val + dfs(node.left) + dfs(node.right)
         
-        return (root.val if L <= root.val <= R else 0) + \
-                self.rangeSumBST(root.left, L, R) + \
-                self.rangeSumBST(root.right, L, R)
+        return dfs(root)
