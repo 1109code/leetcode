fb547eb9436b3def55b2c3cdc6350d155ed5d0b2
1109code <109256753+1109code@users.noreply.github.com>
2022-09-27 23:19:40 +0900

    Time: 71 ms (7.35%), Space: 13.9 MB (32.13%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    val: int = 0
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # 중위 순회 노드 값 누적
+        if root:
+            self.bstToGst(root.right)       # 오른쪽 자식부터
+            self.val += root.val            # val(class 변수)에 root의 val 추가하고
+            root.val = self.val             # root에 val 초기화
+            self.bstToGst(root.left)        # 왼쪽 자식 재귀
+        
+        return root
