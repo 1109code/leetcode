77db7041e0901653b9d81a88de9703c707203af1
1109code <109256753+1109code@users.noreply.github.com>
2022-09-29 01:49:27 +0900

    Time: 52 ms (57.83%), Space: 13.8 MB (99.40%) - LeetHub

--- 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.py
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.py
@@ -5,18 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    prev = -sys.maxsize
-    result = sys.maxsize
-
-    # 재귀 구조 중위 순회 비교 결과
-    def minDiffInBST(self, root: TreeNode) -> int:
-        if root.left:                                           # 중위순회
-            self.minDiffInBST(root.left)
-            
-        self.result = min(self.result, root.val - self.prev)
-        self.prev = root.val
-            
-        if root.right:
-            self.minDiffInBST(root.right)
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        prev = - sys.maxsize
+        result = sys.maxsize
+        
+        stack = []
+        node = root
+        
+        # 반복 구조 중위 순회 비교 결과
+        while stack or node:                        # 스택 or 노드 만 있어도 반복되게
+            while node:                             # 오른쪽 노드가 없었으면 그냥 통과
+                stack.append(node)
+                node = node.left                    # 왼쪽먼저 계속 stack에 쌓기
                 
-        return self.result
+            node = stack.pop()                      # 가장 마지막 왼쪽 node 부터
+            
+            result = min(result, node.val - prev)   # 비교
+            prev = node.val                         # 현재 노드가 prev가 되고
+            
+            node = node.right                       # right 탐색 위해 node에 입력
+                                                    # 다음에 pop 되는건 부모의 부모
+        return result
